d09649655e7a8648452a10a6439c5a36cff6002d
pixelatedbus <luthfihakimyusra@gmail.com>
2024-04-09 16:40:58 +0700

    added ejs capabillities

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const ejs = require('ejs');
+
 const connectDB = require ("./models/db");
 const User = require('./models/dbusers');
 const Posts = require('./models/dbposts')
@@ -11,9 +13,14 @@ const app = express();
 connectDB()
 // TO DO LIST: try, catch(error) for most
 // mongoDB Validator
+// SIGN UP
 
 //-------------------middleware--------------------------------
-app.use(express.static('features')) // for sendFile();
+// app.use(express.static('features')) // for sendFile(), irrelevant switc to ejs;
+
+app.set('view engine', 'ejs'); //pick ejs as view engine
+
+app.set('views', __dirname + '/features/feature-1') //pick folder for ejs
 
 app.use(session({secret: 'blogsecretkey', resave: false, saveUninitialized: false})); //session middleware
 
@@ -36,7 +43,7 @@ app.use((req, res, next) => {
 
 //------------login---------------
 app.get('/login', (req, res => {
-    res.sendFile(__dirname + '/features/feature-1/login/index.html');
+    res.render('login/index'); // ejs handling
 }))
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
@@ -54,27 +61,27 @@ app.get('/home', (req, res) => {
     res.redirect('/')
 }) //for mistaken people
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'features/feature-1/homepage/index.html');
+    res.render('homepage/index');
 }) //to homepage
 app.delete('/:id', async (req,res) => {
     const blogId = req.params.id;
     await Posts.findByIdAndDelete(blogId) // delete
-    res.redirect('/')
+    res.redirect('/home')
 }) //to delete
 
 //---------------blogs--------------------------
 app.get('/blogs', (req, res) => {
-    res.sendFile(__dirname + 'features/feature-1/blogs/index.html');
+    res.render('blogs/index');
 }) //list of blogs
 
 app.get('/blogs/:id', async(req, res) => {
     const blogId = req.params.id;
-    res.sendFile(__dirname + 'features/feature-1/blogs/index.html' + blogId)
+    res.render('blogs/index',  {id: blogId})
 }) //sends the specific blog Id through a query parameter, that would be checked by javascript in the index.html, fetch with mongodb (WOULD BE EASIER WITH EJS)
 
 //---------------form------------------------
 app.get('/form', (req, res) => {
-    res.sendFile(__dirname + 'features/feature-1/form/index.html');
+    res.render('/form/index');
 }) //send to form
 
 app.post('/form', async (req, res) => {
@@ -96,18 +103,33 @@ app.post('/form', async (req, res) => {
 app.put('/form/:id', async (req, res) => {
     const blogId = req.params.id
     const currentBlog = await Posts.findOne({ _id: blogId })
-    //---------updating VVV-------
+    //---------updating VVV----------------------
     currentBlog.title = req.body.title;
     currentBlog.content = req.body.content;
     currentBlog.short_description = req.body.desc;
-    //
+    //-------------------------------------------
     await currentBlog.save();
     res.redirect('/form')
 }) //edit request form
 
-//---------------profile-----------
+//---------------profile-------------------------------
+app.get('/profile', async(req, res) => {
+    const currentName = req.session.username //get username
+    const user = await User.findOne({ currentName })
+    res.render('/profile/index', {id: user._id}) 
+}) //get profile tab
 
-
+app.put('/profile', async (req, res) => {
+    const currentName = req.session.username
+    const currentUser = await User.findOne({ currentName })
+    //---------updating VVV-----------------------------
+    currentUser.name = req.body.name;
+    currentUser.bio = req.body.bio;
+    //--------------------------------------------------
+    await currentUser.save();
+    res.redirect('/form')
+}) // update profile
+//------------------------------------------------------
 app.listen(PORT, () => {
     console.log("im listening 5500")
 })
