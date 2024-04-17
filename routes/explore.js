ed6ebe28817ce3a8358e7d9e5bbc8efcd8ff69aa
David Lodianto <david.lodianto@gmail.com>
2024-04-17 18:19:45 +0700

    feat: add paginationgit add .

--- routes/explore.js
+++ routes/explore.js
@@ -4,8 +4,19 @@ const User = require("../models/user");
 const Blog = require("../models/blog");
 
 router.get("/", async (req, res) => {
-    const articles= await Blog.find();
-    res.render("explore/explore", {articles});
+    const page = req.query.p || 0;
+    const articlesPerPage = 6;
+    const articles= await Blog
+        .find()
+        .sort({_id: 1})
+        .skip(page*articlesPerPage)
+        .limit(articlesPerPage);
+    var pageCount = await Blog.countDocuments();
+    pageCount = (pageCount-1)/articlesPerPage;
+    const pageInt = parseInt(page);
+    const next_page = pageInt < pageCount ? `/explore?p=${(parseInt(page)+1)}` : '#';
+    const prev_page = pageInt >0 ? `/explore?p=${pageInt-1}` : '#';
+    res.render("explore/explore", {articles: articles, next_page:next_page, prev_page:prev_page});
 }); //to homepage   
 
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,13 @@ const User = require("../models/user");
 const Blog = require("../models/blog");
 
 router.get("/", async (req, res) => {
-    const articles= await Blog.find();
-    res.render("homepage/index", {articles});
+    const articles= await Blog
+        .find()
+        .sort({_id: 1})
+        .limit(3);
+    const username = req.user ? req.user.username : null;
+    res.render("homepage/index", {articles: articles, username: username});
 }); //to homepage   
+
 
 module.exports = router;
